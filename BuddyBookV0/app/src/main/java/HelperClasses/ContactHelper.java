07ec050773cbd1b7cdfc4497ceb7c964329b02de
ruhul_inonity <ruhul.azam1@gmail.com>
2015-06-22 23:43:17 +0600

    retreives data from phone and show in list...on click calls that number.database isnt used till now

---
+++ BuddyBookV0/app/src/main/java/HelperClasses/ContactHelper.java
@@ -0,0 +1,25 @@
+package HelperClasses;
+
+/**
+ * Created by ruhul_000 on 6/22/2015.
+ */
+public class ContactHelper {
+
+    private String name;
+    private String phoneNo;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+
+}
---
+++ BuddyBookV0/app/src/main/java/com/inonity/buddybook/ContactListActivity.java
@@ -0,0 +1,128 @@
+package com.inonity.buddybook;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import CustomAdapter.ContactAdapter;
+import HelperClasses.ContactHelper;
+
+public class ContactListActivity extends Activity implements
+        AdapterView.OnItemClickListener {
+
+    private ListView listView;
+    private List<ContactHelper> list = new ArrayList<ContactHelper>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content_list);
+
+        listView = (ListView) findViewById(R.id.listViewContacts);
+        listView.setOnItemClickListener(this);
+
+        Cursor phones = getContentResolver().query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null,
+                null, null);
+        while (phones.moveToNext()) {
+
+            String name = phones
+                    .getString(phones
+                            .getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+
+            String phoneNumber = phones
+                    .getString(phones
+                            .getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            ContactHelper objContact = new ContactHelper();
+            objContact.setName(name);
+            objContact.setPhoneNo(phoneNumber);
+            list.add(objContact);
+
+        }
+        phones.close();
+
+        ContactAdapter objAdapter = new ContactAdapter(
+                ContactListActivity.this, R.layout.custom_layout_name_pic_display, list);
+        listView.setAdapter(objAdapter);
+
+        if (null != list && list.size() != 0) {
+            Collections.sort(list, new Comparator<ContactHelper>() {
+
+                @Override
+                public int compare(ContactHelper lhs, ContactHelper rhs) {
+                    return lhs.getName().compareTo(rhs.getName());
+                }
+            });
+            AlertDialog alert = new AlertDialog.Builder(
+                    ContactListActivity.this).create();
+            alert.setTitle("");
+
+            alert.setMessage(list.size() + " Contact Found!!!");
+
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            alert.show();
+
+        } else {
+            showToast("No Contact Found!!!");
+        }
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> listview, View v, int position,
+                            long id) {
+        ContactHelper bean = (ContactHelper) listview.getItemAtPosition(position);
+        showCallDialog(bean.getName(), bean.getPhoneNo());
+    }
+
+    private void showCallDialog(String name, final String phoneNo) {
+        AlertDialog alert = new AlertDialog.Builder(ContactListActivity.this)
+                .create();
+        alert.setTitle("Call?");
+
+        alert.setMessage("Are you sure want to call " + name + " ?");
+
+        alert.setButton("No", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alert.setButton2("Yes", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String phoneNumber = "tel:" + phoneNo;
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri
+                        .parse(phoneNumber));
+                startActivity(intent);
+            }
+        });
+        alert.show();
+    }
+}
