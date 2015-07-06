d884cd9868fb38050c91e919a835c6858f51a0ea
l30n13 <mrtonmoy12@gmail.com>
2015-07-07 01:02:25 +0600

    Modified

--- app/src/main/java/CustomAdapter/ContactAdapter.java
+++ app/src/main/java/CustomAdapter/ContactAdapter.java
@@ -19,38 +19,25 @@ import java.util.List;
 
 import HelperClasses.ContactHelper;
 
-public class ContactAdapter extends ArrayAdapter<ContactHelper> {
 
-    private final Context context;
-    private List<ContactHelper> items;
+public class ContactAdapter extends ArrayAdapter<ContactHelper> {
 
     public ContactAdapter(Context context, List<ContactHelper> items) {
         super(context, R.layout.custom_layout_name_pic_display, items);
-        this.context = context;
-        this.items = items;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
+        LayoutInflater inflater = LayoutInflater.from(getContext());
         View view = inflater.inflate(R.layout.custom_layout_name_pic_display, parent, false);
 
-        Bitmap bitmap = null;
-
         TextView textView = (TextView) view.findViewById(R.id.textViewContactName);
-        textView.setText(items.get(position).getName());
+        textView.setText(getItem(position).getName());
 
         ImageView imageView = (ImageView) view.findViewById(R.id.imageViewContactImage);
 
-        //Converting String into Bitmap format
-        /*try {
-            byte[] encodeByte = Base64.decode(items.get(position).getImage(), Base64.DEFAULT);
-            bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
-        } catch (Exception e) {
-            e.getMessage();
-        }*/
-        String image_uri = items.get(position).getImage();
+        String image_uri = getItem(position).getImage();
 
         if (image_uri != null) {
             imageView.setImageURI(Uri.parse(image_uri));
@@ -58,3 +45,4 @@ public class ContactAdapter extends ArrayAdapter<ContactHelper> {
         return view;
     }
 }
+
